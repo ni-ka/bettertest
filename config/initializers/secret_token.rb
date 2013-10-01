2305921aff547ba4f0b33f172d0444ed354dc473
ni-ka <mail@nils-kaiser.de>
2013-10-01 23:38:05 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bettertest::Application.config.secret_key_base = '646b08a77461d4960f326af807a8016f225ee691db98e8cf3fe4a88911df39e80c3c129fae5401422acefcd83890c2b6a493385eb04a8f8a246e8ece47515391'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bettertest::Application.config.session_store :cookie_store, key: '_bettertest_session'
